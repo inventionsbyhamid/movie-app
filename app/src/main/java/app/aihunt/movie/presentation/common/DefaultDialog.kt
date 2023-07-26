a490fac4e4dec01a1dc5f826ff27121cfbb0187c
Hamid Siddiqui <inventionsbyhamid@gmail.com>
2023-07-26 16:18:23 +0530

    add clickable link in dialog

--- app/src/main/java/app/aihunt/movie/presentation/common/DefaultDialog.kt
+++ app/src/main/java/app/aihunt/movie/presentation/common/DefaultDialog.kt
@@ -1,24 +1,50 @@
 package app.aihunt.movie.presentation.common
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import app.aihunt.movie.R
 
 @Composable
 fun DefaultDialog(title: String, onDismiss: () -> Unit, description: String = "") {
+
+    val uriHandler = LocalUriHandler.current
+
+    val styledMessage = messageFormatter(
+        text = description,
+        primary = true
+    )
+
     AlertDialog(
         onDismissRequest = onDismiss,
         title = {
             Text(text = title)
         },
         text = {
-            Text(
-                text = description,
-                style = MaterialTheme.typography.bodyMedium
+            ClickableText(
+                text = styledMessage,
+                style = MaterialTheme.typography.bodyLarge.copy(color = LocalContentColor.current),
+                modifier = Modifier.padding(16.dp),
+                onClick = {
+                    styledMessage
+                        .getStringAnnotations(start = it, end = it)
+                        .firstOrNull()
+                        ?.let { annotation ->
+                            when (annotation.tag) {
+                                SymbolAnnotationType.LINK.name -> uriHandler.openUri(annotation.item)
+                                else -> Unit
+                            }
+                        }
+                }
             )
         },
         confirmButton = {
